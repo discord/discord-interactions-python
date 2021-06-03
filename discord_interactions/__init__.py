c25b45e480eec936a31bf8863a75d763927e48d8
Justin <justinleeong@gmail.com>
2021-06-03 12:52:28 +0800

    Add types for component-based interactions
    This adds the new interaction type of 3 (MESSAGE_COMPONENT) and response types of 6 and 7.

--- discord_interactions/__init__.py
+++ discord_interactions/__init__.py
@@ -9,11 +9,14 @@ from nacl.signing import VerifyKey
 class InteractionType:
     PING = 1
     APPLICATION_COMMAND = 2
+    MESSAGE_COMPONENT = 3
 
 class InteractionResponseType:
     PONG = 1
     CHANNEL_MESSAGE_WITH_SOURCE = 4
     DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
+    DEFERRED_UPDATE_MESSAGE = 6
+    UPDATE_MESSAGE =  7   
 
 class InteractionResponseFlags:
     EPHEMERAL = 1 << 6
