bd03e22f0776cff97e4674a9353794cee3cb0fef
Justin <justinleeong@gmail.com>
2021-06-04 03:36:52 +0800

    Add types and update example, message components. (#6)
    * Add types for component-based interactions
    
    This adds the new interaction type of 3 (MESSAGE_COMPONENT) and response types of 6 and 7.
    
    * Add a second condition in the flask app example for components
    
    * Upgrade version, pyproject.toml
    
    * Version upgrade, main file

--- discord_interactions/__init__.py
+++ discord_interactions/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from functools import wraps
 
@@ -9,11 +9,14 @@ from nacl.signing import VerifyKey
 class InteractionType:
     PING = 1
     APPLICATION_COMMAND = 2
+    MESSAGE_COMPONENT = 3
 
 class InteractionResponseType:
     PONG = 1
     CHANNEL_MESSAGE_WITH_SOURCE = 4
     DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
+    DEFERRED_UPDATE_MESSAGE = 6
+    UPDATE_MESSAGE =  7   
 
 class InteractionResponseFlags:
     EPHEMERAL = 1 << 6
