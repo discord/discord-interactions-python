d0c5b42840067692bcb37dae631dada1d2e6b497
Izhar Ahmad <54180221+nerdguyahmad@users.noreply.github.com>
2021-10-12 21:21:43 +0500

    feat: add autocomplete enums (#8)
    * feat: add autocomplete enums
    
    * fix: remove unused import (#1)
    
    no idea why it was even there, autoimports I guess. 😄

--- discord_interactions/__init__.py
+++ discord_interactions/__init__.py
@@ -2,7 +2,6 @@ __version__ = '0.3.0'
 
 from functools import wraps
 
-from nacl.encoding import HexEncoder
 from nacl.exceptions import BadSignatureError
 from nacl.signing import VerifyKey
 
@@ -10,13 +9,15 @@ class InteractionType:
     PING = 1
     APPLICATION_COMMAND = 2
     MESSAGE_COMPONENT = 3
+    APPLICATION_COMMAND_AUTOCOMPLETE = 4
 
 class InteractionResponseType:
     PONG = 1
     CHANNEL_MESSAGE_WITH_SOURCE = 4
     DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
     DEFERRED_UPDATE_MESSAGE = 6
-    UPDATE_MESSAGE =  7   
+    UPDATE_MESSAGE =  7  
+    APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8 
 
 class InteractionResponseFlags:
     EPHEMERAL = 1 << 6
